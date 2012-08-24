a621995a462a600a8a6784790588009d8a82bc31
Daniel Wagner-Hall <dawagner@gmail.com>
2012-08-24 17:02:23 +0100

    Adding ability to run single test

--- run_tests.py
+++ run_tests.py
@@ -8,4 +8,19 @@ import unittest
 dev_appserver.fix_sys_path()
 
 suites = unittest.loader.TestLoader().discover("src/givabit", pattern="*_test.py")
+
+if len(sys.argv) > 1:
+    def GetTestCases(caseorsuite, acc=None):
+        if acc is None:
+            acc = []
+        if isinstance(caseorsuite, unittest.TestCase):
+            acc.append(caseorsuite)
+            return acc
+        for child in caseorsuite:
+            GetTestCases(child, acc)
+        return acc
+    all_tests = GetTestCases(suites)
+    tests = [test for test in all_tests if test.id() == sys.argv[1]]
+    suites = unittest.TestSuite(tests)
+
 unittest.TextTestRunner(verbosity=1).run(suites)
