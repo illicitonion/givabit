7dbeaf7801c23c6cb2e0329a27a68b432fae069c
Daniel Wagner-Hall <dawagner@gmail.com>
2012-08-27 01:59:28 +0100

    Use instance, not local properties

--- src/givabit/backend/charity_repository_test.py
+++ src/givabit/backend/charity_repository_test.py
@@ -2,25 +2,23 @@ import test_data
 import test_utils
 
 from charity import Charity
-from charity_repository import CharityRepository
 from errors import MissingValueException, MultipleValueException
 
 class CharityRepositoryTest(test_utils.TestCase):
     def setUp(self):
         super(CharityRepositoryTest, self).setUp()
         self.all_charities = [test_data.c1, test_data.c2, test_data.c3, test_data.c4]
-        self.repo = CharityRepository()
         for charity in self.all_charities:
-            self.repo.add_or_update_charity(charity)
+            self.charity_repo.add_or_update_charity(charity)
 
     def test_lists_charities(self):
-        self.assertSequenceEqual(self.repo.list_charities(), self.all_charities)
+        self.assertSequenceEqual(self.charity_repo.list_charities(), self.all_charities)
 
     def test_gets_single_charity(self):
-        self.assertEqual(self.repo.get_charity('Shelter'), test_data.c1)
-        self.assertEqual(self.repo.get_charity('Oxfam'), test_data.c2)
-        self.assertRaises(MissingValueException, lambda: self.repo.get_charity('Does not exist'))
+        self.assertEqual(self.charity_repo.get_charity('Shelter'), test_data.c1)
+        self.assertEqual(self.charity_repo.get_charity('Oxfam'), test_data.c2)
+        self.assertRaises(MissingValueException, lambda: self.charity_repo.get_charity('Does not exist'))
         try:
-            self.repo.get_charity('BHF')
+            self.charity_repo.get_charity('BHF')
         except MultipleValueException, e:
             self.assertSequenceEqual(e.values, [test_data.c3, test_data.c4])
--- src/givabit/backend/user_repository_test.py
+++ src/givabit/backend/user_repository_test.py
@@ -1,7 +1,6 @@
 import test_utils
 
 from user import User, UserStatus
-from user_repository import UserRepository
 
 class UserRepositoryTest(test_utils.TestCase):
     def test_validates_properties(self):
@@ -9,25 +8,23 @@ class UserRepositoryTest(test_utils.TestCase):
 
     def test_creates_unconfirmed_user(self):
         email = 'someone@foo.com'
-        repo = UserRepository()
         new_user = User(email=email)
 
-        repo.create_unconfirmed_user(new_user)
-        self.assertRaises(lambda: repo.get_user(email))
-        returned_user = repo.get_unconfirmed_user(email)
+        self.user_repo.create_unconfirmed_user(new_user)
+        self.assertRaises(lambda: self.user_repo.get_user(email))
+        returned_user = self.user_repo.get_unconfirmed_user(email)
         self.assertEquals(returned_user.email, email)
         self.assertEquals(returned_user.status, UserStatus.UNCONFIRMED)
 
     def test_confirms_user(self):
         email = 'someone@foo.com'
-        repo = UserRepository()
         new_user = User(email=email)
 
-        repo.create_unconfirmed_user(new_user)
-        self.assertRaises(lambda: repo.confirm_user(new_user, 'wrong_code'))
+        self.user_repo.create_unconfirmed_user(new_user)
+        self.assertRaises(lambda: self.user_repo.confirm_user(new_user, 'wrong_code'))
 
         confirmation_code = new_user.confirmation_code
-        repo.confirm_user(new_user, confirmation_code)
-        self.assertEquals(repo.get_user(email), new_user)
+        self.user_repo.confirm_user(new_user, confirmation_code)
+        self.assertEquals(self.user_repo.get_user(email), new_user)
 
-        self.assertRaises(lambda: repo.confirm_user(new_user, confirmation_code))
+        self.assertRaises(lambda: self.user_repo.confirm_user(new_user, confirmation_code))
