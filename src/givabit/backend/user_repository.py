72350090482d512e846d7dc2ffcd007c24005326
Daniel Wagner-Hall <dawagner@gmail.com>
2012-08-27 19:26:02 +0100

    Check confirmation code setting password

--- src/givabit/backend/user_repository.py
+++ src/givabit/backend/user_repository.py
@@ -6,6 +6,9 @@ from errors import IllegalStateException, MissingValueException, MultipleValueEx
 #TODO: Throttle attempts by source and account
 #TODO: bcrypt/similar
 
+class IncorrectConfirmationCodeException(Exception):
+    pass
+
 class UserRepository(object):
     def create_unconfirmed_user(self, user):
         # Creates a user whose account cannot be used without confirmation
@@ -23,18 +26,25 @@ class UserRepository(object):
             raise IllegalStateException('Cannot re-confirm already-confirmed user %s' % user)
         if found_user.confirmation_code == code:
             user.status = UserStatus.VALID
-            user.confirmation_code = None
             user.put()
+        else:
+            raise IncorrectConfirmationCodeException('Could not confirm user %s with code %s' % (user, code))
 
     def get_unconfirmed_user(self, email):
         # Gets a user whose account is not currently active
-        return self._get_user(email, lambda q: q.filter('status =', UserStatus.UNCONFIRMED))
+        return self._get_user(email=email)
 
     def get_user(self, email):
         # Gets a user whose account is currently active
         return self._get_user(email, lambda q: q.filter('status =', UserStatus.VALID))
 
-    def set_password(self, email, password):
+    def set_password(self, email, password, confirmation_code=None):
+        user = None
+        if confirmation_code is not None:
+            user = self.get_unconfirmed_user(email=email)
+            self.confirm_user(user=user, code=confirmation_code)
+        if user is None:
+            raise IllegalStateException('Must provide either confirmation code or existing password to change password for user %s' % user)
         salt = self._generate_salt()
         Password(email=email, salt=salt, hash=self._hash(password, salt), user=self._get_user(email=email)).put()
 
--- src/givabit/backend/user_repository_test.py
+++ src/givabit/backend/user_repository_test.py
@@ -1,6 +1,7 @@
 import test_utils
 
 from user import User, UserStatus
+from user_repository import IncorrectConfirmationCodeException
 
 class UserRepositoryTest(test_utils.TestCase):
     def test_validates_properties(self):
@@ -36,25 +37,49 @@ class UserRepositoryTest(test_utils.TestCase):
         self.assertRaises(lambda: self.user_repo.authenticate(email=email, password=''))
 
     def test_cannot_log_in_if_password_not_set(self):
-        pass
+        email = 'someone@foo.com'
+        password = 'Some_password'
+        new_user = User(email=email)
+        self.user_repo.create_unconfirmed_user(new_user)
+        self.user_repo.confirm_user(new_user, new_user.confirmation_code)
+        self.assertRaises(lambda: self.user_repo.authenticate(email=email, password=''))
 
     def test_preserves_confirmation_code_until_password_set(self):
-        pass
+        email = 'someone@foo.com'
+        password = 'Some_password'
+        new_user = User(email=email)
+        self.user_repo.create_unconfirmed_user(new_user)
+
+        self.user_repo.confirm_user(new_user, new_user.confirmation_code)
+        self.user_repo.confirm_user(new_user, new_user.confirmation_code)
+
+        self.user_repo.set_password(email=new_user.email, password=password, confirmation_code=new_user.confirmation_code)
+        self._assert_can_log_in(new_user, password)
+
+    def test_cannot_set_password_with_wrong_second_confirmation(self):
+        email = 'someone@foo.com'
+        password = 'Some_password'
+        new_user = User(email=email)
+        self.user_repo.create_unconfirmed_user(new_user)
+
+        self.user_repo.confirm_user(new_user, new_user.confirmation_code)
+        self.user_repo.confirm_user(new_user, new_user.confirmation_code)
+
+        self.assertRaises(IncorrectConfirmationCodeException, lambda: self.user_repo.set_password(email=new_user.email, password=password, confirmation_code='wrong'))
 
     def test_can_log_in(self):
         email = 'someone@foo.com'
         password = 'Some_password'
         new_user = User(email=email)
         self.user_repo.create_unconfirmed_user(new_user)
-        self.user_repo.confirm_user(new_user, new_user.confirmation_code)
 
-        self.user_repo.set_password(email=new_user.email, password=password)
-
-        found_user = self.user_repo.authenticate(email=email, password=password)
-
-        self.assertEquals(found_user, new_user)
+        self.user_repo.set_password(email=new_user.email, password=password, confirmation_code=new_user.confirmation_code)
+        self._assert_can_log_in(new_user, password)
 
     def test_sequential_users_probably_get_different_salt(self):
+        pass
+
+    def test_sequential_users_probably_get_different_confirmation_codes(self):
         pass
 
     def test_cannot_set_password_without_confirmation_code_if_unconfirmed(self):
@@ -62,3 +87,8 @@ class UserRepositoryTest(test_utils.TestCase):
 
     def test_cannot_set_password_without_existing_password_if_confirmed(self):
         pass
+
+    def _assert_can_log_in(self, user, password):
+        found_user = self.user_repo.authenticate(email=user.email, password=password)
+        self.assertEquals(found_user, user)
+
